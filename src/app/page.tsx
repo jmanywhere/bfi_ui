bb85cb0881679911a803b328f6b21f98616745c5
Semi Invader <=>
2023-12-29 09:17:29 -0600

    show stats on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-between py-24 px-8 md:px-24">
       <div className="border-2 border-primary rounded-2xl p-10 max-w-3xl">
-        <div className="w-full relative flex flex-col-reverse md:flex-col items-center">
+        <div className="w-full relative flex flex-col-reverse md:flex-col items-center pb-6">
           <div className="w-full relative flex flex-col px-0 md:px-36 justify-center">
             <h1 className="text-5xl font-bold text-center text-primary font-anton mb-4 relative">
               Bonk Fi
@@ -15,6 +15,34 @@ export default function Home() {
           </div>
           <div className=" w-36 h-36 md:absolute relative md:-top-[20px] top-0 left-0">
             <Image src={logo} alt="Logo for Hero card" />
+          </div>
+        </div>
+        <p className="font-poppins text-black">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          finibus odio. Donec finibus arcu ac turpis accumsan rutrum. Etiam
+          sapien tortor,
+        </p>
+        <div className="justify-evenly flex md:flex-row flex-col items-center pt-6 gap-4">
+          <div className="stats drop-shadow">
+            <div className="stat bg-primary">
+              <div className="stat-desc">BFi</div>
+              <div className="stat-value text-white">5M</div>
+              <div className="stat-title text-accent">Total Supply</div>
+            </div>
+          </div>
+          <div className="stats drop-shadow">
+            <div className="stat bg-primary">
+              <div className="stat-desc">BFi</div>
+              <div className="stat-value text-white">4M</div>
+              <div className="stat-title text-accent">Total Staked</div>
+            </div>
+          </div>
+          <div className="stats drop-shadow">
+            <div className="stat bg-primary">
+              <div className="stat-desc">BFi</div>
+              <div className="stat-value text-white">1.2345</div>
+              <div className="stat-title text-accent">Price</div>
+            </div>
           </div>
         </div>
       </div>
