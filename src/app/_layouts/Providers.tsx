ae6ad89f78582a2db35d11014318d1adefb3cf8e
Semi Invader <=>
2024-01-22 00:37:27 -0600

    select 1 from sample

--- src/app/_layouts/Providers.tsx
+++ src/app/_layouts/Providers.tsx
@@ -8,6 +8,7 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
+import sample from "lodash/sample";
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
@@ -16,8 +17,10 @@ export const Providers = (props: { children: ReactNode }) => {
   const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint.
-  const endpoint =
-    process.env.NEXT_PUBLIC_HELIUS_RPC_URL || clusterApiUrl("mainnet-beta");
+  const endpoint = sample([
+    process.env.NEXT_PUBLIC_HELIUS_RPC_URL || clusterApiUrl("mainnet-beta"),
+    process.env.NEXT_PUBLIC_RPC_2 || clusterApiUrl("mainnet-beta"),
+  ]);
 
   const wallets = useMemo(
     () => [new PhantomWalletAdapter()],
